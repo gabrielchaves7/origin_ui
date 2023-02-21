46468ed869a79dff56da17eda87a2341cb521946
Gabriel Chaves <gabrielbum@gmail.com>
2023-02-21 08:44:20 -0300

    Initial layout to home form

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:origin_design_system/assets.dart';
 import 'package:origin_design_system/origin_design_system.dart';
 import 'package:origin_ui/home/cubit/home_cubit.dart';
 import 'package:origin_ui/l10n/l10n.dart';
@@ -24,17 +26,77 @@ class HomeView extends StatelessWidget {
     final l10n = context.l10n;
     return OriginPage(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          RichText(
-            text: TextSpan(
-              text: l10n.title,
-              children: <TextSpan>[
-                TextSpan(
-                  text: l10n.titleBold,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: OriginSpacing.xx),
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: l10n.title,
+                style: OriginTextStyles.subtitle,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: l10n.titleBold,
+                    style: OriginTextStyles.subtitleSemibold,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          OriginCard(
+            child: Column(
+              children: [
+                const OriginIcon(
+                  iconPath: Assets.origin_design_system$assets_origin_icon_circle_svg,
+                ),
+                const SizedBox(height: OriginSpacing.xx),
+                Text(
+                  l10n.homeCardTitle,
+                  textAlign: TextAlign.center,
+                ).headingSmall,
+                Text(
+                  l10n.homeCardSubtitle,
+                  textAlign: TextAlign.center,
+                ).paragraph,
+                const SizedBox(height: OriginSpacing.xx),
+                OriginTextField(
+                  label: l10n.annualIncomeInputLabel,
+                  textInputType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    NumericTextFormatter(),
+                    LengthLimitingTextInputFormatter(11),
+                  ],
+                  icon: const OriginIcon(
+                    iconPath: Assets.origin_design_system$assets_dollar_sign_svg,
+                    size: OriginIconSize.extraSmall,
+                  ),
+                ),
+                const SizedBox(height: OriginSpacing.xx),
+                OriginTextField(
+                  label: l10n.monthlyCostsInputLabel,
+                  textInputType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    NumericTextFormatter(),
+                    LengthLimitingTextInputFormatter(11),
+                  ],
+                  icon: const OriginIcon(
+                    iconPath: Assets.origin_design_system$assets_dollar_sign_svg,
+                    size: OriginIconSize.extraSmall,
+                  ),
+                ),
+                const SizedBox(height: OriginSpacing.xx),
+                SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: const Text('Continue'),
+                  ),
                 ),
               ],
-            ).subtitle,
+            ),
           )
         ],
       ),
